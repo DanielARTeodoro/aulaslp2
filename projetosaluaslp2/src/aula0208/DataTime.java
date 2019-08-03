d8475da3ec8e5822b393c85c81155d738df30d94
Daniel <Daniel@169.254.24.93>
2019-08-02 23:07:44 -0300

    Inicio da aula.

---
+++ projetosaluaslp2/src/aula0208/DataTime.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package aula0208;
+
+/**
+ * @author Daniel
+ *
+ */
+public class DataTime {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
