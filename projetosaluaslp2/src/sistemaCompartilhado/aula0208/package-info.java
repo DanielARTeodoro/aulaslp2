eed1a8e96ae0a5969ee28a3919a9911d8ac600cb
Daniel <Daniel@DARTMover>
2019-08-07 03:11:32 -0300

    Atualização de conteudo.

--- projetosaluaslp2/src/sistemaCompartilhado/aula0208/package-info.java
+++ projetosaluaslp2/src/sistemaCompartilhado/aula0208/package-info.java
@@ -16,6 +16,16 @@ package sistemaCompartilhado.aula0208;
 
 /// https://www.youtube.com/watch?v=vZZJqYNxiLg
 
+/// https://www.youtube.com/watch?v=FhqdMFJbsxs
+
+/// https://www.youtube.com/watch?v=_wTTtZH-C7c
+
+/// https://www.youtube.com/watch?v=AICSqcLdh90
+
+/// https://www.youtube.com/watch?v=ZuweKNNF0_o
+
+/// https://www.youtube.com/watch?v=6Uan34Whx3o
+
 /// ----------------------------------------------------------------------
 
 /// Um momento para Relaxar;
@@ -24,6 +34,6 @@ package sistemaCompartilhado.aula0208;
 
 /// ----------------------------------------------------------------------
 
-/// Um momento para Refrescar o CÃ©rebro;
+/// Um momento para Refrescar o Cérebro;
 
 /// https://www.youtube.com/watch?v=0fGo0EeoAHY
