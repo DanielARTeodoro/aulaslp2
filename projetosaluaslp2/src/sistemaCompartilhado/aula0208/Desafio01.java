d8cd3b3fda6ae066efc902e653a2d29b336b638b
Daniel <Daniel@DARTMover>
2019-08-08 19:54:50 -0300

    Atualização do Desafio 01.

--- projetosaluaslp2/src/sistemaCompartilhado/aula0208/Desafio01.java
+++ projetosaluaslp2/src/sistemaCompartilhado/aula0208/Desafio01.java
@@ -3,6 +3,11 @@
  */
 package sistemaCompartilhado.aula0208;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 /**
  * @author Daniel
  * @author Frank
@@ -22,7 +27,47 @@ public class Desafio01 {
 		/// Pesquisa sendo desenvolvido no siste,a compartilhado.
 		
 		/// Comesando o sistema; 
+		
+		Instant inicio = Instant.now();
 
+		try {
+			Thread.sleep(1000); // fazer o processador esperar
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Instant fim = Instant.now();
+
+		LocalDateTime dt1 = LocalDateTime.now();
+		LocalDateTime dt2 = LocalDateTime.of(2017, 1, 1, 23, 0, 0);
+
+		LocalTime time1 = LocalTime.now();
+		LocalTime time2 = LocalTime.of(23, 0, 0);
+		
+		Duration duracao = Duration.between(inicio, fim);
+		
+		Duration d1 = 
+		Duration.between(Instant.now().plusSeconds(1000), 
+				Instant.now().plusSeconds(1000));
+		
+		Duration d2 = 
+				Duration.between(Instant.now().plusSeconds(1000), 
+						Instant.now().plusSeconds(1000));
+		
+		Duration d3 = 
+				Duration.between(dt1, dt2);
+		
+		Duration d4 = 
+				Duration.between(time1, time2);
+		
+		
+		System.out.println(d1);
+		System.out.println(d2);
+		System.out.println(d3);
+		System.out.println(d4);
+		
+
+	
 	}
 
 }
