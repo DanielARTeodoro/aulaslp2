5fc6118b8d20d55766e6859e2a443344d8e54e47
frank <frank@DESKTOP-O033H0J>
2019-08-30 18:18:58 -0300

    alguma coisa aki em relação ao tema de hoje.

--- projetosaluaslp2/src/frank/package-info.java
+++ projetosaluaslp2/src/frank/package-info.java
@@ -1,1 +1,54 @@
 package frank;
+
+/*
+root{
+	-fx-background-color: #ffffff;
+	-fx-font-size:14px;
+	
+}
+
+TextField{
+	-fx-text-fill: blue;
+	-fx-border-radius: 4x;
+	-fx-font-size: 12px;
+	-fx-cursor: text;
+}
+Blogin{
+	-fx-background-color: #0000d4;
+	-fx-text-fill: #ffffff;
+	-fx-font-size: 16px;
+}
+
+-----
+package util:
+
+public class Rotas{
+	public static String MÉDICO = "/view/medicos.fxml";
+
+	//public static String MÉDICO = "br/com/hospitalif/view/medicos.fxml";//
+
+	public static String PAG1 = "/view/Pag1.fxml";
+	public static String PAG2 = "/view/Pag2.fxml";
+	public static String LOGIN = "/view/Login.fxml";
+
+	app{}
+
+
+----
+
+public class Main extends Application{
+
+	@override
+	public void start(Stage primaryStage){
+	FMXLoader loader = new FXMLLoader(getClass().getResource("Rotas.LOGIN"));
+	Parent root = loader.load();
+	Scene scene = new Scene(root, 600, 400);
+	scene.getStylesheets(getClass().getResource("/css/app.css").toExternalForm);
+	stage.setScene(scene);
+	stage.show();
+
+	}		
+}
+
+*/
+
