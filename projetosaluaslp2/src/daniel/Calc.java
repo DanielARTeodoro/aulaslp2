77f0e4422915a542afec430b0cc50d388f33cf2b
Daniel <Daniel@DARTMover>
2019-09-06 03:21:33 -0300

    Aula.

---
+++ projetosaluaslp2/src/daniel/Calc.java
@@ -0,0 +1,23 @@
+package daniel;
+/**
+ * 
+ */
+
+/**
+ * @author Daniel
+ *
+ */
+public class Calc {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	 int n;
+		n=4/0;
+		System.out.println(n);
+	}
+
+}
